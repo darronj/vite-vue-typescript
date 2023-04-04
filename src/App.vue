6702d0d6f9367f99a6b9594d1188d6c0b9461862
darronj <darronj@users.noreply.github.com>
2023-04-04 13:47:38 -0500

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,11 @@
     <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
     <router-link to="/contact">Contact</router-link>
   </nav>
-  <main class="bg-gray-200">
+  <main>
     <router-view />
   </main>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './style.css';
 </style>
