befb87437a720a4c32bf718990026622a232ce72
Darron Jennings <darronj@outlook.com>
2023-04-04 16:27:48 -0500

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
-    <router-link to="/contact">Contact</router-link>
-  </nav>
-  <main>
-    <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <router-view />
-      <p class="text-gray-500 text-lg">Vue and Tailwind CSS in action</p>
-    </div>
-  </main>
+  <div>
+    <NavBar />
+    <main>
+      <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+        <router-view />
+        <p class="text-gray-500 text-lg">Vue and Tailwind CSS in action</p>
+      </div>
+    </main>
+  </div>
 </template>
+
+<script lang="ts">
+import NavBar from '@/components/NavBar.vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'App',
+  components: {
+    NavBar
+  }
+})
+</script>
 
 <style>
 @import './style.css';
